c0dc165a0551e4ba0fd6d38b364c8734486b3d88
m-danish77 <mmunibdanish456@gmail.com>
2026-06-14 12:00:00 +0500

    Update password validation of zod in authValidation.ts

--- src/validations/authValidation.ts
+++ src/validations/authValidation.ts
@@ -7,8 +7,18 @@ export const registerSchema = z.object({
       .min(2, "Name must be at least 2 characters long"),
     email: z.email({ error: "Please enter a valid email address" }),
     password: z
-      .string({ error: "Password must be a string." })
-      .min(6, "Password must be at least 6 characters long"),
+      .string({
+        error: "Password is required",
+      })
+      .min(8, "Password must be at least 8 characters long")
+      .max(100, "Password is too long")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(
+        /[^A-Za-z0-9]/,
+        "Password must contain at least one special character (e.g., !, @, #, $, %)",
+      ),
     role: z.enum(["student", "instructor", "admin"]).default("student"),
   }),
 });

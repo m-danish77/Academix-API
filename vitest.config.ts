7729fe8afa3ebe1e3fda420e2a6cc9c154d2ea8a
m-danish77 <mmunibdanish456@gmail.com>
2026-06-24 22:20:49 +0500

    testing things

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     environment: "node", // We're testing a Node.js API
     include: ["src/**/*.test.ts"], // Look for test files anywhere in src
-    globals: true, // By this we can use describe, it, expect without importing them!
+    globals: true,
     sequence: {
       concurrent: false, // prevent concurrent test execution to avoid db conflicts
     },
